dec36c9d7e68b8615ebd16b32781eecc97af5047
Dmitri Algazin <dmitri.algazin@hmcts.net>
2025-08-04 15:41:09 +0100

    feat: VP-40, Develop HMCTS Case URN Mapping service; - no all environment variables required in to in repo environments;

--- src/test/java/uk/gov/hmcts/cp/client/CaseUrnMapperClientTest.java
+++ src/test/java/uk/gov/hmcts/cp/client/CaseUrnMapperClientTest.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.cp.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,22 +12,38 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@ActiveProfiles("test")
-@SpringBootTest
 class CaseUrnMapperClientTest {
 
-    @Autowired
     private CaseUrnMapperClient caseUrnMapperClient;
 
-    @MockitoBean
     private RestTemplate restTemplate;
+
+    private final String url = "http://mock-server/mapper";
+    private final String cjscppuid = "mock-cjscppuid";
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = mock(RestTemplate.class);
+        caseUrnMapperClient = new CaseUrnMapperClient(restTemplate) {
+            @Override
+            public String getCaseUrnMapper() {
+                return url;
+            }
+
+            @Override
+            public String getCjscppuid() {
+                return cjscppuid;
+            }
+        };
+    }
 
     @Test
     void shouldBuildJudgesUrlCorrectly() {
-        String sourceId = "CIK2JQKECS";
-        String expectedUrl = "https://steccm64.ingress01.dev.nl.cjscp.org.uk/system-id-mapper-api/rest/systemid/mappings?sourceId=CIK2JQKECS&targetType=CASE_FILE_ID";
+        String sourceId = "SOURCE_ID_123";
+        String expectedUrl = "http://mock-server/mapper?sourceId=SOURCE_ID_123&targetType=CASE_FILE_ID";
 
         String actualUrl = caseUrnMapperClient.buildCaseUrnMapperUrl(sourceId);
         assertThat(actualUrl).isEqualTo(expectedUrl);
@@ -38,8 +51,8 @@ class CaseUrnMapperClientTest {
 
     @Test
     void shouldReturnJudgeDetails_whenRequestSucceeds() {
-        String sourceId = "CIK2JQKECS";
-        String expectedUrl = "https://steccm64.ingress01.dev.nl.cjscp.org.uk/system-id-mapper-api/rest/systemid/mappings?sourceId=CIK2JQKECS&targetType=CASE_FILE_ID";
+        String sourceId = "SOURCE_ID_123";
+        String expectedUrl = "http://mock-server/mapper?sourceId=SOURCE_ID_123&targetType=CASE_FILE_ID";
 
         Map<String, String> body = Map.of("key 1", "value 1");
         ResponseEntity<Object> mockResponse = new ResponseEntity<>(body, HttpStatus.OK);
@@ -59,8 +72,8 @@ class CaseUrnMapperClientTest {
 
     @Test
     void shouldReturnNull_whenRestTemplateThrowsException() {
-        String sourceId = "CIK2JQKECS";
-        String expectedUrl = "https://steccm64.ingress01.dev.nl.cjscp.org.uk/system-id-mapper-api/rest/systemid/mappings?sourceId=CIK2JQKECS&targetType=CASE_FILE_ID";
+        String sourceId = "SOURCE_ID_123";
+        String expectedUrl = "http://mock-server/mapper?sourceId=SOURCE_ID_123&targetType=CASE_FILE_ID";
 
         when(restTemplate.exchange(
                 eq(expectedUrl),
