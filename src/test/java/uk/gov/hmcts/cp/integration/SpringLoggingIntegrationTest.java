7d65882d615bf6a2c05754805083a5c0a4b835fb
coling01 <colin.greenwood@hmcts.net>
2025-09-15 21:39:45 +0100

    feat(logging): add logging and tracing integration tests

--- src/test/java/uk/gov/hmcts/cp/integration/SpringLoggingIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/integration/SpringLoggingIntegrationTest.java
@@ -38,7 +38,7 @@ public class SpringLoggingIntegrationTest {
         });
         assertThat(capturedFields.get("any-mdc-field")).isEqualTo("1234-1234");
         assertThat(capturedFields.get("timestamp")).isNotNull();
-        assertThat(capturedFields.get("logger_name")).isEqualTo("uk.gov.hmcts.cp.logging.SpringLoggingIntegrationTest");
+        assertThat(capturedFields.get("logger_name")).isEqualTo("uk.gov.hmcts.cp.integration.SpringLoggingIntegrationTest");
         assertThat(capturedFields.get("thread_name")).isEqualTo("Test worker");
         assertThat(capturedFields.get("level")).isEqualTo("INFO");
         assertThat(capturedFields.get("message")).isEqualTo("spring boot test message");
