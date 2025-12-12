0b95ef2e68bd0d115204bce72adeade9eadc1827
baran ipek <baran.ipek@hmcts.net>
2025-12-12 15:45:06 +0000

    feat amp-106 update dependency

--- src/main/java/uk/gov/hmcts/cp/Application.java
+++ src/main/java/uk/gov/hmcts/cp/Application.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.cp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
