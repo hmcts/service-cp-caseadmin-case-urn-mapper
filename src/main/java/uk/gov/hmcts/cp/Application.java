70f4543e1f5f0689082c6844121fc91c54259cc2
baran ipek <baran.ipek@hmcts.net>
2025-12-12 15:42:36 +0000

    feat amp-106 update dependency

--- src/main/java/uk/gov/hmcts/cp/Application.java
+++ src/main/java/uk/gov/hmcts/cp/Application.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.cp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor")
