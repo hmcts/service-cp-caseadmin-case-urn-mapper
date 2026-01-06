2e31a50b4459c35e25c48a656ed18013d287bef7
coling01 <colin.greenwood@hmcts.net>
2026-01-06 13:02:27 +0000

    fix: reinstate ignoreCertificates

--- src/main/java/uk/gov/hmcts/cp/client/CaseUrnMapperClient.java
+++ src/main/java/uk/gov/hmcts/cp/client/CaseUrnMapperClient.java
@@ -15,13 +15,6 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -53,7 +46,6 @@ public class CaseUrnMapperClient {
     public ResponseEntity<Object> getCaseFileByCaseUrn(final String sourceId) {
         try {
             final String url = buildCaseUrnMapperUrl(sourceId);
-            ignoreCertificates();
             log.info("get caseId from:{}", url);
             final HttpEntity<String> request = getRequestEntity();
             final ResponseEntity<Object> response = restTemplate.exchange(
@@ -99,31 +91,6 @@ public class CaseUrnMapperClient {
             truncated = input;
         }
         return truncated;
-    }
-
-    private void ignoreCertificates() {
-        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-            @Override
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            @Override
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        }};
-
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-            log.error("Error while ignoring SSL certificate", e);
-        }
     }
 
     public String getCpBackendUrl() {
--- src/main/java/uk/gov/hmcts/cp/config/AppConfig.java
+++ src/main/java/uk/gov/hmcts/cp/config/AppConfig.java
@@ -4,11 +4,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
+        ignoreCertificates();
         return new RestTemplate();
+    }
+
+    private void ignoreCertificates() throws NoSuchAlgorithmException, KeyManagementException {
+        final TrustManager[] trustAllCerts = {new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        final SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 }
