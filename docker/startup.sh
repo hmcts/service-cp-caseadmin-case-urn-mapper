c7a8875113be499d507fe8c923f95007103eeec2
coling01 <colin.greenwood@hmcts.net>
2026-01-09 14:27:53 +0000

    fix: add startup script to import certs in /etc/certs into java keystore
    Temp add certs into repository till we sort the kubernetes mounting them

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env sh
 # Script to add ssl trust certs into the current trust keystore before we start our spring boot app
-# We need to do this because
-# The app will use the certs
+# We use self signed certificates in our dev and test environments so we need to add these to our chain of trust
+# The kubernetes startup will load any self signed certificates into /etc/certs
+# We load any certs found in the /etc/certs into the default truststore
+# The spring boot java app will load the certificates from the default truststore
+function logmsg {
+    SCRIPTNAME=$(basename $0)
+    echo "$SCRIPTNAME : $1"
+}
 
-# Set source folder
-# ( We want to error in Dev but ot in Prod. How to do this ? Is there an env var we can use ? )
-# To set this when running the Docker we can ue "docker run img -e "CERTS_DIR=/etc/certs"
-echo "startup.sh running and loading certificates ..."
+logmsg "startup.sh running and loading certificates ..."
 if [ -z "$CERTS_DIR" ]; then
-    echo "Warning - expects \$CERTS_DIR to be set. i.e. export CERTS_DIR="/etc/certs
-    echo "Defaulting to /etc/certs"
+    logmsg "Warning - expects \$CERTS_DIR to be set. i.e. export CERTS_DIR="/etc/certs
+    logmsg "Defaulting to /etc/certs"
     export CERTS_DIR="/etc/certs"
 fi
 
 export KEYSTORE="$JAVA_HOME/lib/security/cacerts"
 if [ ! -f "$KEYSTORE" ]; then
-    echo "Error - expects keystore to already exist"
+    logmsg "Error - expects keystore to already exist"
     exit 1
 fi
 
 export count=1
-echo "Loading certificates from $CERTS_DIR"
+logmsg "Loading certificates from $CERTS_DIR"
 for FILE in $(ls $CERTS_DIR)
 do
-    alias="nonprod$count"
-    echo "Adding $CERTS_DIR/$FILE to truststore with alias $alias"
+    alias="mojcert$count"
+    logmsg "Adding $CERTS_DIR/$FILE to truststore with alias $alias"
     keytool -importcert -file $CERTS_DIR/$FILE -keystore $KEYSTORE -storepass changeit -alias $alias -noprompt
     count=$((count+1))
 done
@@ -33,19 +36,8 @@ keytool -list -keystore $KEYSTORE -storepass changeit | grep "Your keystore cont
 
 export JARFILE=$(ls /app/*.jar | grep -v 'plain' | head -n1)
 if [ -f "$JARFILE" ]; then
-    echo "Running java jarfile $JARFILE"
+    logmsg "Running java jarfile $JARFILE"
     java -jar $JARFILE
 else
-    echo "No jarfile found in /app"
+    logmsg "No jarfile found in /app"
 fi
-
-# Some useful keytool commands
-#
-# Ignore warning "Warning: use -cacerts option to access cacerts keystore"
-# keytool useful stuff
-# keytool -list -keystore $KEYSTORE -storepass changeit
-# Think the finger print for cpp-nonline is  A0:AF:DB:4F:...:CA:DA:14:C6
-# The may be lots of certs so need to grep
-# keytool -list -keystore $KEYSTORE -storepass changeit | grep "A0:AF:DB"
-# keytool -delete -keystore $KEYSTORE -storepass changeit -alias nonprod1
-# keytool -delete -keystore $KEYSTORE -storepass changeit -alias mykey
